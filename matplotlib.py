f1245f9ba1a0fd21d900e37626d9f8dfe6628a21
Alex.Ash <alex.ah@list.ru>
2018-10-08 14:33:06 -0400

    Work

--- matplotlib.py
+++ matplotlib.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 plt.style.use('ggplot')  # Красивые графики
 plt.rcParams['figure.figsize'] = (20, 10)  # Размер картинок
@@ -7,4 +8,8 @@ plt.rcParams['font.family'] = 'sans-serif'  # Шрифт
 
 # Вывести два графика на основе данных, в которых два столбца и единый индекс
 def two_charts(data, kind: str):
-    data.plot(kind=kind, subplots=True, figsize=(15, 10))
+    data.plot(kind=kind, rot=45, subplots=True, figsize=(15, 10))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Seaborn
